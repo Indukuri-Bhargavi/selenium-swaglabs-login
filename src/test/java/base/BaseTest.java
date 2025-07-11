6b700890b5de2281294776a776c2a64dc59cb1db
unknown <ibhargavi876@gmail.com>
2025-07-11 17:21:26 +0530

    data driven is added with extent report

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,23 +3,19 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-
+import utils.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-  public  WebDriver driver;
+	public WebDriver driver;
 
-    @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
+	@BeforeMethod
+	public void setUp() {
+	    WebDriverManager.chromedriver().setup();
 
-        driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
-    }
+	    driver = new ChromeDriver();
+	    driver.get(ConfigReader.get("url"));
+	    driver.manage().window().maximize();
+	}
 
-    @AfterMethod
-    public void tearDown() {
-       driver.quit();
-    }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,16 +1,26 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExcelUtil;
 
+@Listeners(listeners.TestListener.class)
 public class LoginTest extends BaseTest {
-    @Test
-    public void validLoginTest()  {
+	
+	@DataProvider(name = "loginData")
+	public Object[][] getData() {
+	    return ExcelUtil.readLoginData("test-data/LoginData.xlsx", "Login");
+	}
+
+    @Test(dataProvider = "loginData")
+    public void loginWithMultipleUsers(String username, String password) {
         LoginPage login = new LoginPage(driver);
-        login.login("standard_user", "secret_sauce");
-        Assert.assertEquals(driver.getTitle(), "Swag Labs");
+        login.login(username, password);
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory")); // basic check
     }
 }
